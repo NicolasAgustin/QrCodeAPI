1e466fec6383e8cfd90084d3c4481c8a413890b3
NicolasAgustin <nickrak10@gmail.com>
2022-07-15 23:04:26 -0300

    Added module for database queries

---
+++ modules/Mongo.py
@@ -0,0 +1,16 @@
+from pymongo.mongo_client import MongoClient
+
+
+class Mongo():
+
+    def __init__(this, db_port: str, db_ip: str, db_name: str):
+        this.string_connection = (
+            f'mongodb://{db_ip}:{db_port}'
+        )
+        this.client = MongoClient(this.string_connection)
+        this.db = this.client.get_database(db_name)
+
+    def get_user_by_pass_email(this, passw: str, user: str) -> dict:
+        collection = this.db.get_collection('users')
+        doc = collection.find_one({'user': user, 'password': passw})
+        return doc
