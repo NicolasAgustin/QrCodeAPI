ad3ffcb21cd11724a9aae9b43b878b3eb8aabbd3
NicolasAgustin <nickrak10@gmail.com>
2022-07-19 20:58:41 -0300

    Added endpoint for encode with json body data

--- app.py
+++ app.py
@@ -1,27 +1,54 @@
 import jwt
 
-from decouple import config
 from flask import Flask, jsonify, request
-from modules.Mongo import Mongo
-from modules.auth_middleware import token_required
+from datetime import datetime, timezone, timedelta
+from decouple import config
+from modules.mongo import Mongo
 from modules.qr_generator import generate_qr, decode_qr
+from modules.auth_middleware import token_required
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = config('SECRET_KEY')
-
-DATABASE = Mongo(
-    config('DB_PORT'),
-    config('DB_IP'),
-    config('DB_NAME')
-)
+with app.app_context():
+    app.config['SECRET_KEY'] = config('SECRET_KEY')
+    DATABASE = Mongo(
+        config('DB_PORT'),
+        config('DB_IP'),
+        config('DB_NAME')
+    )
+    app.config['DATABASE'] = DATABASE
 
 
 @app.route('/encode/<text>', methods=['GET'])
 @token_required
 def encode(text: str):
-    return generate_qr(text)
+    try:
+        return generate_qr({
+            'text': text,
+            'fill_color': 'black',
+            'back_color': 'white'
+        })
+    except Exception as e:
+        return jsonify({
+            "message": "Something went wrong!",
+            "error": str(e),
+            "data": None
+        }), 500
+
+
+@app.route('/encode', methods=['POST'])
+@token_required
+def encode_json():
+    try:
+        data = request.json
+        return generate_qr(data)
+    except Exception as e:
+        return jsonify({
+            "message": "Something went wrong!",
+            "error": str(e),
+            "data": None
+        }), 500
 
 
 @app.route('/decode', methods=['POST'])
@@ -56,7 +83,11 @@ def auth():
         token = jwt.encode(
             {
                 'user': validated['user'],
-                'password': validated['password']
+                'password': validated['password'],
+                'exp': (
+                    datetime.now(tz=timezone.utc) +
+                    timedelta(hours=1)
+                )
             },
             app.config['SECRET_KEY'],
             algorithm='HS256'
