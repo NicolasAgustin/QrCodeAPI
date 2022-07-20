58a07b5fd64b5e5fbd09fad74bf36c83b5fbf47d
NicolasAgustin <nickrak10@gmail.com>
2022-07-19 21:00:18 -0300

    Added parameters for encode data

--- modules/qr_generator.py
+++ modules/qr_generator.py
@@ -7,19 +7,31 @@ from PIL import Image
 from flask import Response, send_file
 
 
-def generate_qr(text: str) -> Response:
+def generate_qr(options: dict) -> Response:
+    """ Function to generate the QR image based on the
+        text.
+
+
+    Args:
+        options (dict): Dictionary with the options for generate
+            the QRcode
+
+    Returns:
+        Response: Response
+    """
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=10,
         border=4
     )
-    qr.add_data(text)
+
+    qr.add_data(options['text'])
     qr.make(fit=True)
 
     img: Image = qr.make_image(
-        fill_color='black',
-        back_color='white'
+        fill_color=options['fill_color'],
+        back_color=options['back_color']
     ).convert('RGB')
 
     img_io = BytesIO()
@@ -29,6 +41,19 @@ def generate_qr(text: str) -> Response:
 
 
 def decode_qr(data: bytes):
+    """ Function that receives an image and
+        decode it to obtain the text
+
+    Args:
+        data (bytes): Image as bytes
+
+    Raises:
+        Exception: Raises an exception if it cannot decode
+            QRcode
+
+    Returns:
+        _type_: _description_
+    """
     image = np.asarray(bytearray(data), dtype='uint8')
     img = cv2.imdecode(image, cv2.IMREAD_COLOR)
     qr_detector = cv2.QRCodeDetector()
@@ -39,8 +64,6 @@ def decode_qr(data: bytes):
         raise Exception(
             'No se pudo decodificar el codigo QR provisto.'
         )
-
-    print(data)
 
     return data
 
